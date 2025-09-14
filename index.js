2eb792aaaf0fee3aa876d480cb52db626d5a4742
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-09-14 11:46:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const decreaseBtn=document.getElementById("decreaseBtn");
+const decreaseBtn =document.getElementById("decreaseBtn");
 const resetBtn=document.getElementById("resetBtn");
 const increaseBtn=document.getElementById("increaseBtn");
 const countLabel=document.getElementById("countLabel");
